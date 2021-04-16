e77229a2b58660774bd05001a40d40a81f96c9f8
Ilona Kozak <ilonaikozak@gmail.com>
2021-04-16 15:33:30 +0100

    create variables

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 /* SELECT ITEMS */
+
+const input = document.querySelector('#Input');
+const form = document.querySelector('form');
+const grams = document.querySelector('#gramsOutput');
+const kg = document.querySelector('#kgOutput');
+const oz = document.querySelector('#ozOutput');
+const lbs = document.querySelector('#lbsOutput');
+const cups = document.querySelector('#cupsOutput');
+const spoons = document.querySelector('#spoonsOutput');
+const teaspoons = document.querySelector('#teaspoonsOutput');
+const output = document.querySelector('#output');
+const select = document.querySelector('#select');
