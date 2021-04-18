3e1a0922fe43890d0678a62e088819c3159a56c9
Ilona Kozak <ilonaikozak@gmail.com>
2021-04-18 11:24:23 +0100

    add event listeners

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ const cups = document.querySelector('#cupsOutput');
 const spoons = document.querySelector('#spoonsOutput');
 const teaspoons = document.querySelector('#teaspoonsOutput');
 const output = document.querySelector('#output');
+const section = document.querySelector('.section-center');
 const select = document.querySelector('#select');
 
 /* FUNCTIONS */
@@ -46,9 +47,9 @@ function calcPounds() {
 
 function calcGrams() {
     grams.innerHTML = Number(this.value);
-    kg.innerHTML    = (Number(this.value) / 1000).toFixed(2);
-    oz.innerHTML    = (Number(this.value) * 0.035274).toFixed(2);
-    lbs.innerHTML   = (Number(this.value) / 500).toFixed(2);
+    kg.innerHTML = (Number(this.value) / 1000).toFixed(2);
+    oz.innerHTML = (Number(this.value) * 0.035274).toFixed(2);
+    lbs.innerHTML = (Number(this.value) / 500).toFixed(2);
     cups.innerHTML = (Number(this.value) / 220).toFixed(2);
     spoons.innerHTML = (Number(this.value) * 0.07576).toFixed(2);
     teaspoons.innerHTML = (Number(this.value) * 0.227).toFixed(2);
@@ -56,9 +57,9 @@ function calcGrams() {
 
 function calcOz() {
     grams.innerHTML = (Number(this.value) / 0.035274).toFixed(2);
-    kg.innerHTML    = (Number(this.value) / 35.274).toFixed(2);
-    oz.innerHTML    = Number(this.value);
-    lbs.innerHTML   = (Number(this.value) * 0.062500).toFixed(2);
+    kg.innerHTML = (Number(this.value) / 35.274).toFixed(2);
+    oz.innerHTML = Number(this.value);
+    lbs.innerHTML = (Number(this.value) * 0.062500).toFixed(2);
     cups.innerHTML = (Number(this.value) / 7.76).toFixed(2);
     spoons.innerHTML = (Number(this.value) * 2.12).toFixed(2);
     teaspoons.innerHTML = (Number(this.value) * 6.36).toFixed(2);
@@ -66,9 +67,9 @@ function calcOz() {
 
 function calcKilograms() {
     grams.innerHTML = Number(this.value) * 1000;
-    kg.innerHTML    = Number(this.value);
-    oz.innerHTML    = (Number(this.value) * 35.274).toFixed(2);
-    lbs.innerHTML   = (Number(this.value) * 2.2046).toFixed(2);
+    kg.innerHTML = Number(this.value);
+    oz.innerHTML = (Number(this.value) * 35.274).toFixed(2);
+    lbs.innerHTML = (Number(this.value) * 2.2046).toFixed(2);
     cups.innerHTML = (Number(this.value) * 4.55).toFixed(2);
     spoons.innerHTML = (Number(this.value) * 75.76).toFixed(2);
     teaspoons.innerHTML = (Number(this.value) * 227.27).toFixed(2);
@@ -76,9 +77,9 @@ function calcKilograms() {
 
 function calcCups() {
     grams.innerHTML = Number(this.value) * 220;
-    kg.innerHTML    = (Number(this.value) / 4.55).toFixed(2);
-    oz.innerHTML    = (Number(this.value) * 7.76).toFixed(2);
-    lbs.innerHTML   = (Number(this.value) * 2.2046).toFixed(2);
+    kg.innerHTML = (Number(this.value) / 4.55).toFixed(2);
+    oz.innerHTML = (Number(this.value) * 7.76).toFixed(2);
+    lbs.innerHTML = (Number(this.value) * 2.2046).toFixed(2);
     cups.innerHTML = Number(this.value);
     spoons.innerHTML = (Number(this.value) * 16.67).toFixed(2);
     teaspoons.innerHTML = Number(this.value) * 50;
@@ -86,9 +87,9 @@ function calcCups() {
 
 function calcSpoons() {
     grams.innerHTML = (Number(this.value) / 0.07576).toFixed(2);
-    kg.innerHTML    = (Number(this.value) / 75.76).toFixed(2);
-    oz.innerHTML    = (Number(this.value) * 7.76).toFixed(2);
-    lbs.innerHTML   = (Number(this.value) / 34.3628).toFixed(2);
+    kg.innerHTML = (Number(this.value) / 75.76).toFixed(2);
+    oz.innerHTML = (Number(this.value) * 7.76).toFixed(2);
+    lbs.innerHTML = (Number(this.value) / 34.3628).toFixed(2);
     cups.innerHTML = (Number(this.value) / 16.67).toFixed(2);
     spoons.innerHTML = Number(this.value);
     teaspoons.innerHTML = Number(this.value) * 3;
@@ -96,11 +97,26 @@ function calcSpoons() {
 
 function calcTeaspoons() {
     grams.innerHTML = (Number(this.value) / 0.227).toFixed(2);
-    kg.innerHTML    = (Number(this.value) / 227.27).toFixed(2);
-    oz.innerHTML    = (Number(this.value) / 6.36).toFixed(2);
-    lbs.innerHTML   = (Number(this.value) / 102.95).toFixed(2);
+    kg.innerHTML = (Number(this.value) / 227.27).toFixed(2);
+    oz.innerHTML = (Number(this.value) / 6.36).toFixed(2);
+    lbs.innerHTML = (Number(this.value) / 102.95).toFixed(2);
     cups.innerHTML = (Number(this.value) / 50).toFixed(2);
     spoons.innerHTML = (Number(this.value) / 3).toFixed(2);
     teaspoons.innerHTML = Number(this.value);
 }
 
+/* EVENT LISTENERS */
+
+input.addEventListener('input', function (e) {
+    output.style.visibility = 'visible';
+    section.style.height = 'auto';
+}
+);
+input.addEventListener('input', calcPounds);
+select.addEventListener('input', pickUnit);
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log('form submission submission cancelled')
+});
